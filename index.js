a2e7759378c2808fa8ceecbb0c40d0dcf3f406ab
Amos Koech <koechronix@yahoo.com>
2022-06-18 22:36:11 -0400

    commit tree b2f891161c9603773678ea4470a80200591f5f80

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
+
+function reverse(word){
+  const wordArray = word.split("");
+  const reverseWordArray = wordArray.reverse();
+  const reverseWord = reverseWordArray.join("");
+  return reverseWord;
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord = reverse(word); 
+  if (word ===reverseWord){
+    return true;
+  } else {
+    return false;
+  }
+
 }
+
+    // reverse the input string
+
+    // if the reversed string is the same as the input
+  
+  
+    // create an array from the input string
+    // reverse the array
+  
+    // create a string from the reversed array
+
+    // return the reversed string
+    
+
+
 
 /* 
   Add your pseudocode here
+  
 */
 
 /*
